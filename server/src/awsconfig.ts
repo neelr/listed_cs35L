e99f2e70f7289713b25ae00538069b017f454c20
verybigballoon <bogdanyr4@gmail.com>
2024-05-13 21:24:01 -0700

    Merged DynamoDB changes

---
+++ server/src/awsconfig.ts
@@ -0,0 +1,3 @@
+export const dynamodbconfig = {
+  endpoint: "",
+};
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,12 @@
 // basic express typescript server
-import express from 'express';
-import bodyParser from 'body-parser';
-import { Request, Response } from 'express';
-import addTask from './routes/addTask';
-import getTasks from './routes/getTasks';
-import addUser from './routes/addUser';
-import dotenv from 'dotenv';
+import express from "express";
+import bodyParser from "body-parser";
+import { Request, Response } from "express";
+import addTask from "./routes/addTask";
+import getTasks from "./routes/getTasks";
+import addUser from "./routes/addUser";
+import dotenv from "dotenv";
+import { createUser, getUser } from "./dynamodbtest";
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
@@ -14,15 +15,37 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.post('/add-task', addTask);
-app.post('/add-user', addUser);
+app.post("/add-task", addTask);
+app.post("/add-user", addUser);
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Listed Server is running!');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Listed Server is running!");
 });
 
-app.get('/get-tasks', getTasks);
+app.get("/asdas", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.post("/dynamodb", async (req: Request, res: Response) => {
+  console.log(req.body);
+  const response = await createUser({
+    name: req.body.name,
+    userID: req.body.userID,
+  });
+  console.log(response);
+  res.send(response);
+});
+
+app.get("/dynamodb", async (req: Request, res: Response) => {
+  const response = await getUser({
+    userID: req.body.userID,
+  });
+  console.log(response);
+  res.send(response);
+});
+
+app.get("/get-tasks", getTasks);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
