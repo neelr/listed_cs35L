7927cb04158d3d1e3f7f3a0c0abc2ea840acc211
schirica7 <stefanchirica2@gmail.com>
2024-05-16 17:57:28 -0700

    Login Screen Done
    Possibly run npm install again

--- Listed/App.tsx
+++ Listed/App.tsx
@@ -1,22 +1,47 @@
 import React from 'react';
 import HomeScreen from './screens/HomeScreen';
-import NextScreen from './screens/NextScreen';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
+import LoginScreen from './screens/LoginScreen';
+import { useFonts,
+  InknutAntiqua_300Light,
+  InknutAntiqua_400Regular,
+  InknutAntiqua_500Medium,
+  InknutAntiqua_600SemiBold,
+  InknutAntiqua_700Bold,
+  InknutAntiqua_800ExtraBold,
+  InknutAntiqua_900Black,
+} from '@expo-google-fonts/inknut-antiqua';
+import AppLoading from 'expo-app-loading';
 
 export type RootStackParamList = {
   Home: undefined;
-  Next: undefined;
+  Login: undefined;
 };
+
+export 
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const App: React.FC = () => {
+  const [fontsLoaded] = useFonts({
+    InknutAntiqua_300Light,
+    InknutAntiqua_400Regular,
+    InknutAntiqua_500Medium,
+    InknutAntiqua_600SemiBold,
+    InknutAntiqua_700Bold,
+    InknutAntiqua_800ExtraBold,
+    InknutAntiqua_900Black,
+  });
+
+  if (!fontsLoaded) {
+    return null
+  }
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Home'>
         <Stack.Screen name='Home' component={HomeScreen} options={{ headerShown: false }}></Stack.Screen>
-        <Stack.Screen name='Next' component={NextScreen} options={{ headerShown: false }}></Stack.Screen>
+        <Stack.Screen name='Login' component={LoginScreen} options={{ headerShown: false }}></Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
