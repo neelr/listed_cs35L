c0b78d08b39e397498d4df58c39ef4d7c7b9d04d
Elio Merolle <elioamerolle@gmail.com>
2024-06-04 15:36:55 -0700

    does not work yet

--- Listed/constants.ts
+++ Listed/constants.ts
@@ -1,17 +1,29 @@
-import axios from "axios";
-import * as Yup from "yup";
-import * as SecureStore from "expo-secure-store";
+import * as Yup from 'yup';
 
 export const PASSWORD_SCHEMA = Yup.string()
-  .required("Password is required")
-  .matches(/[0-9]/, "Password must contain a number")
-  .matches(/[a-z]/, "Password must contain a lowercase letter")
-  .matches(/[A-Z]/, "Password must contain an uppercase letter")
-  .matches(
-    /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/,
-    "Password must contain a special character"
-  )
-  .matches(/.{8,}/, "Password must contain at least 8 characters");
+  .required('Password is required')
+  .test('password-complexity', 'Password must contain at least 8 characters, one uppercase letter, one lowercase letter, one number, and one special character', function (value) {
+    if (!value) return false;
+
+    const errors = [];
+    if (!/[0-9]/.test(value)) {
+      errors.push('Password must contain a number');
+    }
+    if (!/[a-z]/.test(value)) {
+      errors.push('Password must contain a lowercase letter');
+    }
+    if (!/[A-Z]/.test(value)) {
+      errors.push('Password must contain an uppercase letter');
+    }
+    if (!/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/.test(value)) {
+      errors.push('Password must contain a special character');
+    }
+    if (value.length < 8) {
+      errors.push('Password must be at least 8 characters long');
+    }
+    return errors.length > 0 ? this.createError({ message: errors.join(', ') }) : true;
+  });
+
 
 export const API_DNS = "ec2-35-95-43-181.us-west-2.compute.amazonaws.com";
 
